fc379f00bd9208c2f9e586b75ad0b523422f31eb
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-10-22 06:34:54 -0300

    Corrigindo histórico para remover segredo da API

---
+++ criar_db.py
@@ -0,0 +1,36 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+PASTA_BASE = 'base'
+def criar_db():
+    # carregar documentos
+    # Dividir os textos em Chunks(Partes do documento)
+    #vetorizar os chunks com o processo de embedding
+    documentos = carregar_documentos()
+    chunks = dividir_chunks(documentos)
+    vetorizar_chunks(chunks)
+def carregar_documentos():
+    carregador = PyPDFDirectoryLoader(PASTA_BASE, glob="*.pdf")
+    documentos = carregador.load()
+    return documentos
+
+def dividir_chunks(documentos):
+    separador_documentos = RecursiveCharacterTextSplitter(
+        chunk_size = 2000,
+        chunk_overlap = 500,
+        length_function = len,
+        add_start_index = True
+    )
+    chunks = separador_documentos.split_documents(documentos)
+    print(len(chunks))
+    return chunks
+
+def vetorizar_chunks(chunks):
+    db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory='database')
+    print("Banco de dados criado com sucesso")
+criar_db()
---
+++ main.py
@@ -0,0 +1,46 @@
+# pip install python-dotenv langchain langchain-openai langchain-community langchain-chroma chromadb openai pypdf 
+from langchain_chroma.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CAMINHO_DB = 'database'
+
+prompt_template = """ Responda a pergunta do usuario 
+{pergunta}
+
+Com base nessas informacoes:
+
+{base_conhecimento} abaixo
+
+
+"""
+def perguntar():
+    pergunta = input("Escreva sua pergunta")
+
+    #Carregar o banco dados
+    funcao_embedding = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CAMINHO_DB, embedding_function=funcao_embedding)
+    #Comparar a pergunta do usuario (embedding)com meu banco de dados
+
+    resultados = db.similarity_search_with_relevance_scores(pergunta,k=4)
+    if len(resultados) == 0 or resultados[0][1] < 0.7:
+        print("Nao conseguiu encontrar alguma informacao relevante na base")
+        return
+    textos_resultado =[]
+    for resultado in resultados:
+        texto = resultado[0].page_content  
+        textos_resultado.append(texto)
+
+    base_conhecimento = "\n\n----\n\n".join(textos_resultado)
+    prompt = ChatPromptTemplate.from_template(prompt_template)
+    prompt = prompt.invoke({"pergunta":pergunta, "base_conhecimento": base_conhecimento})
+    
+
+    modelo=ChatOpenAI()
+    textos_resultado = modelo.invoke(prompt).content
+    print("Resposta da IA" , textos_resultado)
+perguntar()
